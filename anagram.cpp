2f11dc1b06c61a481c00fa85e239e69d2d7bc609
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-05-07 00:18:44 -0400

    make anagrams better and more concise

--- anagram.cpp
+++ anagram.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<int> smap(26);        
-        for(int i = 0; i < s.length(); ++i){
-            smap.at(s.at(i) - 97)++;
+        for(auto& i : s){
+            smap.at(i - 'a')++;
         }
-        for(int i = 0; i < t.length(); ++i){
-            smap.at(t.at(i) - 97)--;
+        for(auto& i : t){
+            smap.at(i - 'a')--;
         }
-        for(int i = 0; i < smap.size(); i++){
-            if(smap.at(i) != 0) return false;
+        for(auto& i : smap){
+            if(i != 0) return false;
         }
         return true;   
     }
