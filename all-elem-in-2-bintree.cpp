ef50db064bc136875f92445c8abf4e38d909c1ff
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-05-20 23:25:29 -0400

    simplified lol

--- all-elem-in-2-bintree.cpp
+++ all-elem-in-2-bintree.cpp
@@ -11,23 +11,24 @@
  */
 #include <algorithm>
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+    vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+    {
         vector<int> out;
         returnAll(out, root1);
         returnAll(out, root2);
         sort(out.begin(), out.end());
         return out;
-        
     }
-    
-    void returnAll(vector<int>& v, TreeNode* tree){
-        if (tree == nullptr) return;
-        TreeNode* cur = tree;
-        returnAll(v, cur->left);
-        v.push_back(cur->val);
-        returnAll(v, cur->right);
-        return;
+
+    void returnAll(vector<int> &v, TreeNode *tree)
+    {
+        if (tree == nullptr)
+            return;
+        returnAll(v, tree->left);
+        v.push_back(tree->val);
+        returnAll(v, tree->right);
     }
 };
