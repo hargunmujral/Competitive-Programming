af6a2a1dbf68994098e764326567aee8aa616f5f
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-05-21 00:49:00 -0400

    did fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,3 +1,28 @@
+// iterative fibonacci
+
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int a[] = {0, 1, 1};
+        int temp = a[2];
+        for (int i = 2; i < n; i++)
+        {
+            temp = a[1] + a[2];
+            a[0] = a[1];
+            a[1] = a[2];
+            a[2] = temp;
+        }
+        return temp;
+    }
+};
+
+// recursive fibonacci
 class Solution
 {
 public:
