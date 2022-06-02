62b1535a45f1041992aa06625b9edd2e9a4d1cac
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-06-02 18:31:15 -0400

    added another solution for jewels and stones

--- jewels-and-stones.cpp
+++ jewels-and-stones.cpp
@@ -1,13 +1,31 @@
 class Solution
 {
 public:
-    int numJewelsInStones(string jewels, string stones)
+    class Solution
     {
-        int out = 0;
-        for (const auto &i : jewels)
+    public:
+
+        // more memory efficient solution, O(n*m) time
+
+        // int numJewelsInStones(string jewels, string stones) {
+        //     int out = 0;
+        //     for(const auto& i : jewels){
+        //         out += count(stones.begin(), stones.end(), i);
+        //     }
+        //     return out;
+        // }
+
+        // more time efficient, O(n) + O(m) time
+        int numJewelsInStones(string jewels, string stones)
         {
-            out += count(stones.begin(), stones.end(), i);
+            int out = 0;
+            unordered_set<int> set(jewels.begin(), jewels.end());
+            for (const auto &j : stones)
+            {
+                if (set.count(j))
+                    out++;
+            }
+            return out;
         }
-        return out;
-    }
+    };
 };
