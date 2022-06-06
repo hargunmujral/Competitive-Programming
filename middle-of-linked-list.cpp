ca3c24eacd2160c65f2443d9aa1c242eaf63e241
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-06-06 16:56:15 -0400

    included optimized solution

--- middle-of-linked-list.cpp
+++ middle-of-linked-list.cpp
@@ -26,4 +26,21 @@ public:
             slow = slow->next;
         return slow;
     }
+
+    // Optimized Solution:
+    ListNodeO *middleNodeOptimized(ListNode *head)
+    {
+        if (head == nullptr)
+            return head;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
 };
+
+
