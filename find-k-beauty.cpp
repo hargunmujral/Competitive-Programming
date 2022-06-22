e96aba6a6ac49079072a5bf299d672ec60e4ebc7
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-06-22 10:24:41 -0700

    optimized solution for find k beauty

--- find-k-beauty.cpp
+++ find-k-beauty.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
 public:
+    // first attempt at question
     int divisorSubstrings(int num, int k)
     {
         int count = 0;
@@ -12,6 +13,25 @@ public:
             if (divisor && num % divisor == 0)
                 count++;
             end++;
+        }
+        return count;
+    }
+
+    // does not use string copying, thus more memory efficient
+    int divisorSubstringsOptimized(int num, int k)
+    {
+        int count = 0;
+        int check = num;
+        long window = 1;
+        for (int i = 0; i < k; i++)
+            window *= 10;
+        int divisor;
+        while (num >= window / 10)
+        {
+            divisor = num % window;
+            if (divisor && check % divisor == 0)
+                count++;
+            num /= 10;
         }
         return count;
     }
