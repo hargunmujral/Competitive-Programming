01b54733e6045ed6636b6529873d361116037e03
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-07-04 06:55:56 -0700

    altered code for differnce of two arrays

--- difference-of-two-arrays.cpp
+++ difference-of-two-arrays.cpp
@@ -3,14 +3,14 @@ class Solution
 public:
     vector<vector<int>> findDifference(vector<int> &nums1, vector<int> &nums2)
     {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
+        unordered_set<int> set1;
+        unordered_set<int> set2;
         for (const auto &i : nums1)
-            if (count(nums2.begin(), nums2.end(), i))
-                set1.erase(i);
+            if (!count(nums2.begin(), nums2.end(), i))
+                set1.insert(i);
         for (const auto &i : nums2)
-            if (count(nums1.begin(), nums1.end(), i))
-                set2.erase(i);
+            if (!count(nums1.begin(), nums1.end(), i))
+                set2.insert(i);
         vector<int> out1(set1.begin(), set1.end());
         vector<int> out2(set2.begin(), set2.end());
         return {out1, out2};
