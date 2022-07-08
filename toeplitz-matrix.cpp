f5af06b24ff42e22dc7344e44bd5537d27960794
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-07-08 09:10:58 -0700

    created an optimized solution for toeplitz matrix

--- toeplitz-matrix.cpp
+++ toeplitz-matrix.cpp
@@ -1,6 +1,15 @@
 class Solution
 {
 public:
+    // optimized solution
+    bool isToeplitzMatrixOptimized(vector<vector<int>> &matrix) {
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = 0; j < matrix[0].size(); j++)
+                if (i > 0 && j > 0 && matrix[i][j] != matrix[i - 1][j - 1])
+                    return false;
+        return true;
+    }
+    // worse solution
     bool diagonalSearch(vector<vector<int>> &matrix, pair<int, int> pos, int val)
     {
         while (pos.first < matrix.size() && pos.second < matrix[0].size())
