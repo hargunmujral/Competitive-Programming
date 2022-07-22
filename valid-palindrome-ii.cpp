dbc0d54c6fb39230b9c36a2099b869f375980c91
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-07-22 17:44:46 -0400

    completed valid palindrome

---
+++ valid-palindrome-ii.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    bool isPal(string s, int start, int end)
+    {
+        while (start < end)
+            if (s[start++] != s[end--])
+                return false;
+        return true;
+    }
+    bool validPalindrome(string s)
+    {
+        int start = 0;
+        int end = s.size() - 1;
+        while (start < end)
+        {
+            if (s[start] != s[end])
+                return isPal(s, start, end - 1) || isPal(s, start + 1, end);
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
