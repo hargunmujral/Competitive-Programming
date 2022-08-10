9c015df0e3392cc7dc67b22bda75d901a429fd49
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-09 20:32:35 -0400

    created optimized solution for perfect number

--- perfect-number.cpp
+++ perfect-number.cpp
@@ -11,4 +11,11 @@ public:
         }
         return num == 0;
     }
+    bool checkPerfectNumberOptimized(int num) {
+        int constNum = num;
+        for (int i = 1; i * i <= constNum; i++)
+            if (constNum % i == 0)
+                (num -= i) -= (i * i != constNum) ? (constNum / i) : 0;
+        return num == -constNum;
+    }
 };
