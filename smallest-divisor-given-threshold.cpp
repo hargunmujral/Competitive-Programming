ad64188b48e2183d35759d5f1be634afb7b7fe28
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-15 16:35:41 -0400

    completed find smallest divisor given a threshold

---
+++ smallest-divisor-given-threshold.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int smallestDivisor(vector<int> &nums, int threshold)
+    {
+        int left = 1, right = pow(10, 6), sum = 0, mid;
+        while (left < right)
+        {
+            sum = 0;
+            mid = (left + right) / 2;
+            for (const auto &i : nums)
+                sum += i / mid + (i % mid != 0);
+            if (sum > threshold)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+};
