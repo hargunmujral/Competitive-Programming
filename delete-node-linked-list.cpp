eccebf45aee17340b3baa19f55691692d5206120
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-16 17:21:40 -0400

    completed delete linked list node

---
+++ delete-node-linked-list.cpp
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        *node = *node->next;
+    }
+};
+// this was a poorly designed question, can be made a lot better
