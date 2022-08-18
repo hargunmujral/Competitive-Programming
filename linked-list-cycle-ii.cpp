a0e5cbd2e2eec17f7a9f6bbe4d366d8562f42a3c
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-18 17:47:17 -0400

    completed more memory optimized solution for linked list cycle ioi

--- linked-list-cycle-ii.cpp
+++ linked-list-cycle-ii.cpp
@@ -13,4 +13,16 @@ public:
         }
         return nullptr;
     }
+    ListNode *detectCycleMemoryOptimized(ListNode *head)
+    {
+        while (head != nullptr)
+        {
+            if (head->val == INT_MAX)
+                return head;
+            head->val = INT_MAX;
+            head = head->next;
+        }
+        return nullptr;
+    }
+
 };
