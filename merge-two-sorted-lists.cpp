c2c6a67638ef2ecfc0b5551bdec46de91379287d
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-08-23 21:52:26 -0700

    one lined merge two sorted lists

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -15,4 +15,10 @@ public:
         }
         
     }
+    ListNode* mergeTwoListsOneLine(ListNode* list1, ListNode* list2) {
+        return !list1 ? list2 : !list2                
+        ? list1 : list1->val < list2->val ? 
+        (list1->next = mergeTwoLists(list1->next, list2), list1)
+        : (list2->next = mergeTwoLists(list1, list2->next), list2);
+    }
 };
