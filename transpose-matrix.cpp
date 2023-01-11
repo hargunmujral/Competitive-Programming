a3ab595d81c58f290e77816fc2fe20a3473b31a5
hargunmujral <hargunmujral@gmail.com>
2023-01-10 23:50:24 -0500

    Completed transpose matrix

---
+++ transpose-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix)
+    {
+        vector<vector<int>> output(matrix[0].size(), vector<int>(matrix.size(), 0));
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                output[j][i] = matrix[i][j];
+            }
+        }
+        return output;
+    }
+};
