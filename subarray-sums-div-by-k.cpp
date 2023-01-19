76c9d090615390ad7ebf6e243342a186909f575a
hargunmujral <hargunmujral@gmail.com>
2023-01-19 14:09:55 -0500

    Removed unnecessary function and import

--- subarray-sums-div-by-k.cpp
+++ subarray-sums-div-by-k.cpp
@@ -1,14 +1,8 @@
 #include <vector>
-#include <iostream>
 
 class Solution
 {
 public:
-    int sumUpTo(int n)
-    {
-        return (n * (n - 1)) / 2;
-    }
-
     int subarraysDivByK(vector<int> &nums, int k)
     {
         int n = nums.size();
